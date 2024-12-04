e0be050b9f7801c64a01494fe31fc796bbb84c9b
Yiftach Samocha <yiftachsamo@gmail.com>
2024-12-04 17:45:27 +0200

    Fix json import...

--- api/block/block.service.js
+++ api/block/block.service.js
@@ -42,7 +42,7 @@ async function update(block) {
 }
 
 async function _createData() {
-	const data = JSON.parse(await readFile(new URL('./data.json', import.meta.url), 'utf-8'));
+	const data = JSON.parse(await readFile(new URL('../../data/data.json', import.meta.url), 'utf-8'));
 	const collection = await dbService.getCollection('block')
 	await collection.insertMany(data)
 }
