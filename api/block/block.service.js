dd00798bdbeeddee993621f8f3939b9a9903d332
Yiftach Samocha <yiftachsamo@gmail.com>
2024-12-04 17:39:31 +0200

    Fix json import

--- api/block/block.service.js
+++ api/block/block.service.js
@@ -1,11 +1,7 @@
 import { ObjectId } from 'mongodb'
 import { logger } from '../../services/logger.service.js'
 import { dbService } from '../../services/db.service.js'
-import fs from 'fs';
-import path from 'path';
-
-const DATA = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../data/block.data.json'), 'utf-8'));
-
+import { readFile } from 'fs/promises';
 
 
 export const blockService = {
@@ -46,6 +42,7 @@ async function update(block) {
 }
 
 async function _createData() {
+	const data = JSON.parse(await readFile(new URL('./data.json', import.meta.url), 'utf-8'));
 	const collection = await dbService.getCollection('block')
-	await collection.insertMany(DATA)
+	await collection.insertMany(data)
 }
